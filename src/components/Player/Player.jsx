ebaadce7735bf960043e77c9cd63bc24733cb680
delissgt <delissgt@gmail.com>
2024-04-01 11:45:06 -0600

    Added Component Player
    One Way for Edit Player Name Logic Code

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,13 +1,27 @@
+import {useState} from 'react';
 
-export default  function Player({name, symbol}) {
+
+export default function Player({name, symbol}) {
+  const [isEditing, setIsEditing] = useState(false);
+  // const [name, setName] = useState(name);
+
+  function clickEditName(){
+    setIsEditing(true);
+  }
+
 
   return (
       <li>
         <span className='player'>
-          <span className='player-name'>{name}</span>
+          {
+            isEditing
+                ? <input type='text' required  value={name}/>
+                : <span className='player-name' >{name}</span>
+          }
+
           <span className='player-symbol'>{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={clickEditName}>{ isEditing ? "Save" : "Edit"}</button>
       </li>
   );
 }
