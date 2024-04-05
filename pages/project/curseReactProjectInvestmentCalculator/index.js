d61b426be1a2ae22a3a520f8f649435aad9b30de
delissgt <delissgt@gmail.com>
2024-04-05 16:47:29 -0600

    Handing Events & Using Two Way Binding

--- pages/project/curseReactProjectInvestmentCalculator/index.js
+++ pages/project/curseReactProjectInvestmentCalculator/index.js
@@ -1,11 +1,13 @@
 
 import './index.css';
 import HeaderCalculator from "../../../src/components/HeaderCalculator";
+import UserInputCalculator from "../../../src/components/UserInputCalculator";
 
 export default function curseReactProjectInvestmentCalculator() {
   return (
       <>
         <HeaderCalculator title="React Investment Calculator"/>
+        <UserInputCalculator/>
       </>
 
 
