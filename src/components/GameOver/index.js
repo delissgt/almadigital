ad28f7656d1c14f8e82bafea2f938c04546981b6
delissgt <delissgt@gmail.com>
2024-04-05 11:35:12 -0600

    Fix Typo

--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -3,7 +3,7 @@ export default function GameOver({winner, restart}) {
       <div id="game-over">
         <h2>Game Over!</h2>
         {winner && <p>{winner} won!</p>}
-        {!winner && <p>It's a draw</p>}
+        {!winner && <p>It`&apos;`s a draw</p>}
         <p>
           <button onClick={restart}>Rematch!</button>
         </p>
