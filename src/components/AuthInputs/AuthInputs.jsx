0618ab263fa9032318f4ff9eee46e1953991dfc2
delissgt <delissgt@gmail.com>
2024-04-16 16:41:50 -0600

    Dynamic & Conditional Styling with Styled Components

--- src/components/AuthInputs/AuthInputs.jsx
+++ src/components/AuthInputs/AuthInputs.jsx
@@ -15,16 +15,16 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${ ({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
 `
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${ ({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db' };
+  color: ${ ({$invalid}) => $invalid ? '#ef4444' : '#374151' };
+  border: 1px solid ${ ({$invalid}) => $invalid ? '#f73f3f' : 'transparent' } ;
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -52,29 +52,27 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-      {/*<div className="controls">*/}
-        <p>
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
+        <p className='paragraph'>
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
+            $invalid={emailNotValid}
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
             // }}
-            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
-        <p>
-          <Label>Password</Label>
+        <p className='paragraph'>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
+            $invalid={passwordNotValid}
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
         </p>
-      {/*</div>*/}
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
