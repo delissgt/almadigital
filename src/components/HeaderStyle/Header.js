1aa4904b0636a58508c7eb88624e96af5789ed1e
delissgt <delissgt@gmail.com>
2024-04-17 14:53:08 -0600

    Changing Style Using Tailwindcss

--- src/components/HeaderStyle/Header.js
+++ src/components/HeaderStyle/Header.js
@@ -13,8 +13,8 @@ export default function Header() {
 
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
-      <img src={logo.src} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <img src={logo.src} alt="A canvas" className='object-contain mb-8 w-44 h-44'/>
+      <h1 className='text-4xl font-semibold trancking-widest text-center uppercase text-amber-800'>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
       {/* todo problems with nextjs */}
       {/*<p className={`${styles.paragraph}`}>A community of artists and art-lovers.</p>*/}
