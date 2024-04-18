f987e5741f1f99440137a658cea579623100d922
delissgt <delissgt@gmail.com>
2024-04-17 18:49:36 -0600

    Migrating The Demo App to Tailwind

--- src/components/AuthInputs/AuthInputs.jsx
+++ src/components/AuthInputs/AuthInputs.jsx
@@ -1,27 +1,6 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 import Button from "../ButtonAuthInput";
 import Input from "../Input";
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
-
-
-
-
-const TextButton = styled.button`
-  color: #f0b322;
-  border: none;
-  
-  &:hover {
-    color: #f0920e;
-  }
-`
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -44,8 +23,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className="flex flex-col gap-2 mb-6 ">
         <Input
             label='Email'
             invalid={emailNotValid}
@@ -63,13 +42,11 @@ export default function AuthInputs() {
               handleInputChange('password', event.target.value)
             }
           />
-      </ControlContainer>
-      <div className="actions">
-        <TextButton>
-        {/*<button type="button" className="text-button">*/}
+      </div>
+      <div className="flex justify-end gap-4 ">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
-        {/*</button>*/}
-        </TextButton>
+        </button>
         <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
