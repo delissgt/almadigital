a280875e042e9123be5a24b3912e52911738fdd4
delissgt <delissgt@gmail.com>
2024-05-20 11:50:54 -0600

    Adding Challenges to the Demo Project

--- pages/project/curseReactGameFinalCountdown/index.js
+++ pages/project/curseReactGameFinalCountdown/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import Player from '../../../src/components/PlayerFinalCountDown/Player';
+import TimerChallenge from "../../../src/components/TimerChallenge";
 
 
 export default function IndexGameFinalCountDown() {
@@ -12,7 +13,12 @@ export default function IndexGameFinalCountDown() {
             <p>Stop the timer once you estimate that time is (almost) up</p>
           </header>
           <Player />
-          <div id="challenges"></div>
+          <div id="challenges">
+            <TimerChallenge title="Easy" targetTime={1} />
+            <TimerChallenge title="Not Easy" targetTime={5} />
+            <TimerChallenge title="Getting tough" targetTime={10} />
+            <TimerChallenge title="Pros only" targetTime={15} />
+          </div>
         </div>
 
       </>
